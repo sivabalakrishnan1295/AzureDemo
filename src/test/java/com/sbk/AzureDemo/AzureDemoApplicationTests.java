3a6e4b3707fbf2f01149a3eeb02bd1eb07e2773b
sivabalakrishnan1295 <sivabalakrishnan1295@gmail.com>
2021-08-27 20:52:42 +0530

    first commit

---
+++ src/test/java/com/sbk/AzureDemo/AzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbk.AzureDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
